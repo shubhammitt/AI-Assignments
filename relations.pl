5a398627a5a654a92a81c051eb4e7ccfbfe89f5f
Shubham Mittal <shubham18101@iiitd.ac.in>
2020-09-09 23:26:10 +0530

    working

--- relations.pl
+++ relations.pl
@@ -4,6 +4,11 @@ male(shubham).
 male(yogesh).
 male(suresh).
 male(pankaj).
+male(dev).
+male(tekchand).
+male(ashok).
+male(deepu).
+
 
 female(seema).
 female(radha).
@@ -11,6 +16,10 @@ female(mansi).
 female(suman).
 female(gyanwati).
 female(mamta).
+female(anchal).
+female(sarla).
+female(meenu).
+female(muniya).
 
 parent(yogesh, mansi).
 parent(yogesh, radha).
@@ -26,6 +35,18 @@ parent(mamta, dev).
 parent(pankaj, dev).
 parent(pankaj, anchal).
 parent(gyanwati, mamta).
+parent(tekchand, seema).
+parent(tekchand, mamta).
+parent(ashok, meenu).
+parent(ashok, deepu).
+parent(ashok, muniya).
+parent(sarla, meenu).
+parent(sarla, deepu).
+parent(sarla, muniya).
+parent(gyanwati, ashok).
+parent(tekchand, ashok).
+
+
 
 child(mansi, yogesh).
 child(radha, yogesh).
@@ -41,10 +62,22 @@ child(anchal, mamta).
 child(dev, mamta).
 child(anchal, pankaj).
 child(dev, pankaj).
+child(mamta, tekchand).
+child(seema, tekchand).
+child(meenu, sarla).
+child(muniya, sarla).
+child(deepu, sarla).
+child(meenu, ashok).
+child(muniya, ashok).
+child(deepu, ashok).
+child(ashok, gyanwati).
+child(ashok, tekchand).
 
 spouse(yogesh, seema).
 spouse(suresh, suman).
 spouse(pankaj, mamta).
+spouse(gyanwati, tekchand).
+spouse(ashok, sarla).
 
 is_spouse(X, Y) :- spouse(X, Y).
 is_spouse(X, Y) :- spouse(Y, X),!.
@@ -140,12 +173,12 @@ is_son_in_law(Son_in_law, Parent) :- male(Son_in_law),
 
 % rules for father-in-law
 is_father_in_law(Father_in_law, Child_in_law) :- male(Father_in_law),
-										  is_spouse(Spouse, Child_in_law),
+										  is_spouse(Child_in_law,Spouse),
 										  is_father(Father_in_law, Spouse).
 
 % rules for mother-in-law
 is_mother_in_law(Mother_in_law, Child_in_law) :- female(Mother_in_law),
-										  is_spouse(Spouse, Child_in_law),
+										  is_spouse(Child_in_law,Spouse),
 										  is_mother(Mother_in_law, Spouse).
 
 
@@ -198,10 +231,116 @@ print_Mother(X, Y) :- forall(is_mother(X, Y), (write(X), write(" is mother of ")
 
 :- print_Mother(_, _).
 
+print_Son(X, Y) :- forall(is_son(X, Y), (write(X), write(" is son of "), writeln(Y))),nl,nl.
+
+:- print_Son(_, _).
+
+
+print_Daughter(X, Y) :- forall(is_daughter(X, Y), (write(X), write(" is daughter of "), writeln(Y))),nl,nl.
+
+:- print_Daughter(_, _).
+
+
+print_Husband(X, Y) :- forall(is_husband(X, Y), (write(X), write(" is husband of "), writeln(Y))),nl,nl.
+
+:- print_Husband(_, _).
+
+
+print_Wife(X, Y) :- forall(is_wife(X, Y), (write(X), write(" is wife of "), writeln(Y))),nl,nl.
+
+:- print_Wife(_, _).
+
+
+
+print_Sibling(X, Y) :- forall(is_sibling(X, Y), (write(X), write(" is sibling of "), writeln(Y))),nl,nl.
+
+:- print_Sibling(_, _).
+
+print_Brother(X, Y) :- forall(is_brother(X, Y), (write(X), write(" is brother of "), writeln(Y))),nl,nl.
+
+:- print_Brother(_, _).
+
+
+print_Sister(X, Y) :- forall(is_sister(X, Y), (write(X), write(" is sister of "), writeln(Y))),nl,nl.
+
+:- print_Sister(_, _).
+
+
+print_Grandfather(X, Y) :- forall(is_grandfather(X, Y), (write(X), write(" is grandfather of "), writeln(Y))),nl,nl.
+
+:- print_Grandfather(_, _).
+
+
+print_Grandmother(X, Y) :- forall(is_grandmother(X, Y), (write(X), write(" is grandmother of "), writeln(Y))),nl,nl.
+
+:- print_Grandmother(_, _).
+
+
+print_Grandpa(X, Y) :- forall(is_grandpa(X, Y), (write(X), write(" is grandpa of "), writeln(Y))),nl,nl.
+
+:- print_Grandpa(_, _).
+
+
+print_Grandma(X, Y) :- forall(is_grandma(X, Y), (write(X), write(" is grandma of "), writeln(Y))),nl,nl.
+
+:- print_Grandma(_, _).
 
 
 
 
 
-% 
-% :- son(X,Y), write(X), write(" is son of "),write(Y).
+print_Daughter_in_law(X, Y) :- forall(is_daughter_in_law(X, Y), (write(X), write(" is daughter-in-law of "), writeln(Y))),nl,nl.
+
+:- print_Daughter_in_law(_, _).
+
+
+print_Son_in_law(X, Y) :- forall(is_son_in_law(X, Y), (write(X), write(" is son-in-law of "), writeln(Y))),nl,nl.
+
+:- print_Son_in_law(_, _).
+
+
+print_Father_in_law(X, Y) :- forall(is_father_in_law(X, Y), (write(X), write(" is father-in-law of "), writeln(Y))),nl,nl.
+
+:- print_Father_in_law(_, _).
+
+
+print_Mother_in_law(X, Y) :- forall(is_mother_in_law(X, Y), (write(X), write(" is mother-in-law of "), writeln(Y))),nl,nl.
+
+:- print_Mother_in_law(_, _).
+
+
+print_Uncle(X, Y) :- forall(is_uncle(X, Y), (write(X), write(" is uncle of "), writeln(Y))),nl,nl.
+
+:- print_Uncle(_, _).
+
+
+print_Aunt(X, Y) :- forall(is_aunt(X, Y), (write(X), write(" is aunt of "), writeln(Y))),nl,nl.
+
+:- print_Aunt(_, _).
+
+
+print_Cousin(X, Y) :- forall(is_cousin(X, Y), (write(X), write(" is cousin of "), writeln(Y))),nl,nl.
+
+:- print_Cousin(_, _).
+
+
+print_Nephew(X, Y) :- forall(is_nephew(X, Y), (write(X), write(" is nephew of "), writeln(Y))),nl,nl.
+
+:- print_Nephew(_, _).
+
+
+
+
+print_Neice(X, Y) :- forall(is_neice(X, Y), (write(X), write(" is neice of "), writeln(Y))),nl,nl.
+
+:- print_Neice(_, _).
+
+
+print_G_Uncle(X, Y) :- forall(is_granduncle(X, Y), (write(X), write(" is granduncle of "), writeln(Y))),nl,nl.
+
+:- print_G_Uncle(_, _).
+
+
+print_G_Aunt(X, Y) :- forall(is_grandaunt(X, Y), (write(X), write(" is grandaunt of "), writeln(Y))),nl,nl.
+
+:- print_G_Aunt(_, _).
