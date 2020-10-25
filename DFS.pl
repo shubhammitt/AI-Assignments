9bf95569f538121f7994edb91c91d5165d71f641
Shubham Mittal <shubham18101@iiitd.ac.in>
2020-10-25 21:38:05 +0530

    Working DFS

--- DFS.pl
+++ DFS.pl
@@ -1,8 +1,34 @@
+:- retractall(edges(_,_,_)), retractall(predecessor(_,_)).
 :- consult("edges.pl").
 
-pushFront(Item, List, [Item | List]) .
+
 getFirstElement([H| _], H).
 
+addAssert(_, []).
+addAssert(S, [H|T]) :- 
+	retractall(predecessor(_, H)),
+	asserta(predecessor(S, H)),
+	addAssert(S, T).
+
+retracepath(Destination, Source, Cost) :-
+	(
+		(
+			\+ predecessor(_, Destination), 
+			writeln(Source),
+			write("Cost is "),
+			write(Cost),
+			writeln(" KMs"),
+			!
+		);
+		(
+			predecessor(X, Destination),
+			edges(X, Destination, C),
+			NewCost is Cost + C,
+			write(Destination),
+			write("<--"),
+			retracepath(X, Source, NewCost)
+		)
+	).
 
 dfs(Source, Destination, Open, Closed) :-
 	(
@@ -12,22 +38,23 @@ dfs(Source, Destination, Open, Closed) :-
 			!
 		);
 		(
-			getFirstElement(Open, S),
-			select(S, Open, NewOpen),
-			append([S], Closed, NewClosed),
+			getFirstElement(Open, S),  %get first node from Open
+			select(S, Open, NewOpen),  %remove first node from Open
+			append([S], Closed, NewClosed), %insert first node in Closed
 			findall(Successor, edges(S, Successor, _), ListOfSuccessors),
 			subtract(ListOfSuccessors, NewClosed, ListOfUnvisitedSuccessors),
 			(
 				(
 					member(Destination, ListOfUnvisitedSuccessors),
 					writeln("Path Found"),
+					addAssert(S, [Destination]),
+					retracepath(Destination, Source, 0),
 					!
 				);
 				(
 					reverse(ListOfUnvisitedSuccessors, X),
 					append(X, NewOpen, NewOpen1),
-					writeln(NewOpen1),
-
+					addAssert(S, X),
 					dfs(Source, Destination, NewOpen1, NewClosed)
 				)
 
@@ -39,5 +66,4 @@ dfs(Source, Destination, Open, Closed) :-
 
 
 
-:- dfs("Agartala", "Calicut",  ["Agartala"], []).
-:- retractall(edges(_,_,_)).
+:- dfs("Agartala", "Hubli",  ["Agartala"], []).
