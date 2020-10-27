30c7d92926a2159d04242e697691566b37eb14af
Shubham Mittal <shubham18101@iiitd.ac.in>
2020-10-27 16:03:10 +0530

    Submitted

--- main.pl
+++ main.pl
@@ -4,14 +4,14 @@
 
 
 indexOf([Element | _], Element, 0) :- !.
-indexOf([_|Tail], Element, Index) :-
+indexOf([_ | Tail], Element, Index) :-
 	indexOf(Tail, Element, Index1),
 	!,
 	Index is Index1 + 1.
 
 
 getMinElement(Open, H, Destination) :- 
-	findall(Heurstic_Cost, ( member(S, Open), heuristic(S ,Destination , Heurstic_Cost)), List_of_heurstic_cost_to_destination),
+	findall(Heurstic_Cost, (member(S, Open), heuristic(S, Destination , Heurstic_Cost)), List_of_heurstic_cost_to_destination),
 	min_list(List_of_heurstic_cost_to_destination, M),
 	indexOf(List_of_heurstic_cost_to_destination, M, Index),
 	nth0(Index, Open, H1),
@@ -19,7 +19,8 @@ getMinElement(Open, H, Destination) :-
 
 
 setLink(_, []).
-setLink(S, [H|T]) :- 
+setLink(S, [H | T]) :- 
+	retractall(predecessor(_, H)),
 	asserta(predecessor(S, H)),
 	setLink(S, T).
 
@@ -46,8 +47,8 @@ retracepath(Destination, Source, Cost) :-
 
 
 
-bfs(Source, Source, _, _) :- writeln("You are at Destination only!"), !.
-bfs(Source, Destination, Open, Closed) :-
+search(Source, Source, _, _) :- writeln("You are at Destination only!"), !.
+search(Source, Destination, Open, Closed, Choice) :-
 	Source \= Destination,
 	(
 		(
@@ -56,9 +57,17 @@ bfs(Source, Destination, Open, Closed) :-
 			!
 		);
 		(
-			getMinElement(Open, S, Destination),								% get node, S from Open with minimum cost to Destination 
-			select(S, Open, Open1),  											% remove first node, S from Open
-			append(Closed, [S], Closed1),										% insert first node, S in Closed
+			(
+				(
+					Choice = 2,
+					getMinElement(Open, S, Destination)							% get node, S from Open with minimum cost to Destination (Best First Search)
+				);
+				(
+					nth0(0, Open, S)											% get first node, S from Open
+				)
+			),
+			select(S, Open, Open1),  											% remove node, S from Open
+			append(Closed, [S], Closed1),										% insert node, S in Closed
 
 			findall(Successor, edges(S, Successor, _), ListOfSuccessors), 		% generate all successors of S
 			subtract(ListOfSuccessors, Closed1, ListOfUnvisitedSuccessors),		% remove all successors which are already visited
@@ -72,73 +81,38 @@ bfs(Source, Destination, Open, Closed) :-
 				);
 				(
 					reverse(ListOfUnvisitedSuccessors, ListOfUnvisitedSuccessors1),
-					append(ListOfUnvisitedSuccessors1, Open1, Open2),			% append successors in front of Open
-					bfs(Source, Destination, Open2, Closed1)					% repeat
+					(
+						(
+							Choice = 3,
+							append(Open1, ListOfUnvisitedSuccessors1, Open2)	% append successors in end of Open (Breadth First Search)
+						);
+						(
+							append(ListOfUnvisitedSuccessors1, Open1, Open2)	% append successors in front of Open
+						)
+					),
+					search(Source, Destination, Open2, Closed1, Choice)			% repeat
 				)
 			)
 		)
 	).
-
-
-dfs(Source, Source, _, _) :- writeln("You are at Destination only!"), !.
-dfs(Source, Destination, Open, Closed) :-
-	Source \= Destination,
-	(
-		(
-			Open = [], 
-			writeln("No path found!"),
-			!
-		);
-		(
-			nth0(0, Open, S),													% get first node, S from Open
-			select(S, Open, Open1),  											% remove first node, S from Open
-			append(Closed, [S], Closed1),										% insert first node, S in Closed
-
-			findall(Successor, edges(S, Successor, _), ListOfSuccessors), 		% generate all successors of S
-			subtract(ListOfSuccessors, Closed1, ListOfUnvisitedSuccessors),		% remove all successors which are already visited
-			(
-				setLink(S, ListOfUnvisitedSuccessors),							% Set link from successors of S to S
-				(
-					member(Destination, ListOfUnvisitedSuccessors), 			% some successor of S is Destination
-					writeln("Path Found!"),						
-					retracepath(Destination, Source, 0),						% retrace back the path
-					!
-				);
-				(
-					reverse(ListOfUnvisitedSuccessors, ListOfUnvisitedSuccessors1),
-					append(ListOfUnvisitedSuccessors1, Open1, Open2),			% append successors in front of Open
-					dfs(Source, Destination, Open2, Closed1)					% repeat
-				)
-			)
-		)
-	).
-
 
 
 query :- 
 	writeln("1.) for Depth First Search"),	
 	writeln("2.) for Best First Search"),
+	writeln("3.) for Breadth First Search"),
 	writeln("Enter Your Choice :"),
 	read(Choice),
 	(
 		(
-			Choice = 1,
+			member(Choice, [1, 2, 3]),
 			writeln("Enter Source :"),
 			read(Source),
 			writeln("Enter Destination :"), 
 			read(Destination),
-			dfs(Source, Destination, [Source], [])
+			search(Source, Destination, [Source], [], Choice)
 		);
 		(
-			Choice = 2,
-			writeln("Enter Source :"),
-			read(Source),
-			writeln("Enter Destination :"), 
-			read(Destination),
-			bfs(Source, Destination, [Source], [])
-		);
-		(
-			Choice > 2,
 			query
 		)
 	).
